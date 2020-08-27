e5c9954d059c2ffa71d2028b3a53cf0267b0b549
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-08-27 10:43:44 +0100

    More notes

--- R/combine_test_versions.R
+++ R/combine_test_versions.R
@@ -22,41 +22,43 @@
 #' )
 #'
 #' test_b <- tribble(
-#'   ~B1, ~B2, ~B3, ~B4,
+#'   ~Q1, ~Q2, ~Q3, ~Q4,
 #'   1,   1,   1,   0,
 #'   0,   1,   0,   1,
 #'   0,   1,   0,   0
 #' )
 #'
 #' meta_data <- tribble(
-#'   ~A, ~B,
-#'   1,  1,
-#'   2,  3,
-#'   3,  NA,
-#'   4,  2,
-#'   NA,  2
+#'   ~A,   ~B,
+#'   "A1", "Q1",
+#'   "A2", "Q3",
+#'   "A3", NA,
+#'   "A4", "Q2",
+#'   NA,   "Q4"
 #' )
 #'
 #' combine_test_versions(test_a, test_b, meta_data)
 #'
 combine_test_versions <- function(test_a, test_b, meta_data){
 
-  response_data = tibble::tibble(
-    A1_B1 = c(1, 0, 1, 1),
-    A2_B3 = c(1, 1, 1, 1),
-    A3_B0 = c(1, 0, 0, 1),
-    A4_B2 = c(0, 1, 1, 0),
-    A0_B4 = c(1, 0, 1, 1)
+  response_data = tibble::tribble(
+    ~A1_B1, ~A2_B3, ~A3_B0, ~A4_B2, ~A0_B4,
+    0,      1,      1,      0,      NA,
+    0,      0,      1,      1,      NA,
+    0,      1,      0,      1,      NA,
+    1,      1,      NA,     1,      0,
+    0,      0,      NA,     1,      1,
+    0,      0,      NA,     1,      0
   )
 
   meta_data = tibble::tibble(
-    label       = c("A1_B1", "A2_B3", "A3_B0", "A4_B2", "A0_B4"),
+    item_name   = c("A1_B1", "A2_B3", "A3_B0", "A4_B2", "A0_B4"),
     id          = rep(NA, 5),
     group       = rep(NA, 5),
     A           = c(1, 2, 3, 4, 0),
     B           = c(1, 3, 0, 2, 4),
     test_a_name = c("A1", "A2", "A3", "A4", "A0"),
-    test_b_name = c("B1", "B2", "B0", "B2", "B4"),
+    test_b_name = c("Q1", "Q2", "Q0", "Q2", "Q4"),
     status      = c("unchanged", "moved", "removed", "moved", "added")
   )
 
